bdad8d6334f28b8fd1b040b92447526545750006
jose1428 <nguyenvanthi168@gmail.com>
2023-03-19 17:56:25 +0900

    Updates

---
+++ precache-manifest.c48b61e5294d4babb2277f30cf4c7d6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6917321d1cca94402800958901364a14",
+    "url": "/jose142857/index.html"
+  },
+  {
+    "revision": "83e6b0c3aad591ce3d17",
+    "url": "/jose142857/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "11929d5b00b90c195008",
+    "url": "/jose142857/static/css/main.28ab2b4b.chunk.css"
+  },
+  {
+    "revision": "83e6b0c3aad591ce3d17",
+    "url": "/jose142857/static/js/2.7f78a682.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/jose142857/static/js/2.7f78a682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11929d5b00b90c195008",
+    "url": "/jose142857/static/js/main.7c5bc97b.chunk.js"
+  },
+  {
+    "revision": "dbb8910e9682e8b1faa5",
+    "url": "/jose142857/static/js/runtime-main.1dc39f07.js"
+  }
+]);
